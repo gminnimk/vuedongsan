7451804a2f24b9b547d09a45cc85a88072174827
gminnimk <tvt1245@gmail.com>
2025-03-07 23:35:14 +0900

    feat: 예시 데이터.js 파일 & 매물 매칭 모달 상세 페이지 추가

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,33 @@
 <script setup>
 import {ref} from 'vue';
+import rooms from '@/rooms.js'
 
 const menus = ['Home', 'Shop', 'About'];
 
-const oneRooms = [
-  {title: '서울특별시', price: '60만원', reportCount: ref(0)},
-  {title: '강원특별자치도', price: '60만원', reportCount: ref(0)},
-  {title: '부산광역시', price: '60만원', reportCount: ref(0)}
-]
+const isModalOpen = ref(false);
 
-const increase = (i) => {
-  oneRooms[i].reportCount.value += 1;
+const selectedIndex = ref(0); // 선택된 원룸의 번호를 저장 할 변수s
+
+const openModal = (i) => {
+  selectedIndex.value = i;
+  isModalOpen.value = true;
+}
+
+const closeModal = () => {
+  isModalOpen.value = false;
 }
 
 </script>
 
 <template>
+  <div class="black-bg" v-if="isModalOpen">
+    <div class="white-bg">
+      <img :src="rooms[selectedIndex].image" style="width:300px">
+      <h4> {{ rooms[selectedIndex].title }} </h4>
+      <p> {{ rooms[selectedIndex].content }} </p>
+      <button @click="closeModal">닫기</button>
+    </div>
+  </div>
 
   <div class="menu">
     <a v-for="menu in menus" :key="menu">
@@ -23,16 +35,23 @@ const increase = (i) => {
     </a>
   </div>
 
-  <div v-for="(room,i) in oneRooms" :key="i" class="room-container">
-    <h4>{{ room.title }}</h4>
-    <p> {{ room.price }}</p>
-    <button @click="increase(i)">허위매물신고</button>
-    <span>신고수 : {{ room.reportCount }}</span>
+  <div v-for="(room,i) in rooms" :key="i" class="room-container">
+    <img :src="room.image" style="width:500px">
+    <h4 @click="openModal(i)"> {{ room.title }} </h4>
+    <p> {{ room.price }} </p>
+    <p> {{ room.content }} </p>
   </div>
-
 </template>
 
 <style scoped>
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
 .menu {
   background: darkslateblue;
   padding: 15px;
@@ -47,5 +66,30 @@ const increase = (i) => {
 
 .room-container {
   text-align: center;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.white-bg {
+  width: 600px;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+  text-align: center;
+}
+
+button {
+  margin-top: 10px;
+  padding: 5px 10px;
+  cursor: pointer;
 }
 </style>
